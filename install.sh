5b3d8559971d0e628b97d14fec044b73e813a2b5
lu <nonglu1998@qq.com>
2022-07-08 20:23:48 +0800

    update install.sh

--- install.sh
+++ install.sh
@@ -4,3 +4,4 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 nvim +PlugInstall +qall
+export GOPROXY=https://goproxy.cn && nvim +:GoInstallBinaries

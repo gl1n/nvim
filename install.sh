8cf008838e6a161ca09847ccee5f53c46dabe644
NongLu <39052367+gl1n@users.noreply.github.com>
2022-07-09 15:59:31 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,5 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-nvim +PlugInstall +"CocInstall coc-clangd coc-pyright" +qall
-export GOPROXY=https://goproxy.cn && nvim +GoInstallBinaries
+export GOPROXY=https://goproxy.cn
+nvim +PlugInstall +GoInstallBinaries
